0d128c0252da79b1454e458e2688ee0aa062b7b0
girum32 <girum32@gmail.com>
2025-02-10 20:57:38 -0600

    Banner created

--- netflixClone2025/src/components/Banner/Banner.jsx
+++ netflixClone2025/src/components/Banner/Banner.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import axios from "../../utils/axios";
+import requests from "../../utils/requests";
 
 import "./Banner.css";
 
@@ -6,23 +8,37 @@ function Banner() {
   const [movie, setMovie] = useState({});
 
   useEffect(() => {
-    async () => {
+    (async () => {
       try {
         const request = await axios.get(requests.fetchNetflixOriginals);
+
+        console.log(request);
         setMovie(
           request.data.results[
-            Math.floar(Math.random() * request.data.results.length)
+            Math.floor(Math.random() * request.data.results.length)
           ]
         );
       } catch (error) {
         console.log("error", error);
       }
-    };
-  });
+    })();
+  }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
   return (
-    <div className="banner">
-      <div className="banner_content">
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner_contents">
         <h1 className="banner_title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
--- netflixClone2025/src/pages/Home/Home.jsx
+++ netflixClone2025/src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
 
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   );
